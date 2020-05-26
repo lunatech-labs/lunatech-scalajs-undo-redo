d6cb9bbe333aa52f282bfdbd7a2d5462d4fc9044
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2020-05-26 13:48:53 +0200

    Display list of recipes

--- src/main/scala/example/RecipesView.scala
+++ src/main/scala/example/RecipesView.scala
@@ -5,15 +5,18 @@ import scalatags.JsDom.all._
 
 class RecipesView(recipes: ModelRO[Recipes], dispatch: Dispatcher) {
 
+  val r2 = Recipe("r2", "n2", Seq("ig12", "ig22"), Seq("is12", "is22"))
+  val r3 = Recipe("r3", "n3", Seq("ig13", "ig23"), Seq("is13", "is23"))
+
   def render = {
     div(
       h3("Cookbook"),
-      recipes().recipes.map { recipe => 
-        p("Recipe: ", b(recipe.name)),
-      },
+      p("Recipes: "),
+      ul(recipes().recipes.map { recipe => li(recipe.name) }),
       div(
         cls := "btn-group",
-//        button(cls := "btn btn-default", onclick := (() => dispatch(ReversibleRecipeActions.Add(3))), "Increase"),
+        button(cls := "btn btn-default", onclick := (() => dispatch(ReversibleRecipeActions.Add(r2))), "Add r2"),
+        button(cls := "btn btn-default", onclick := (() => dispatch(ReversibleRecipeActions.Add(r3))), "Add r3"),
         button(cls := "btn btn-default", onclick := (() => dispatch(ReversibleRecipeActions.Reset)), "Reset"),
         button(cls := "btn btn-default", onclick := (() => dispatch(ReversibleRecipeActions.Undo)), "Undo"),
         button(cls := "btn btn-default", onclick := (() => dispatch(ReversibleRecipeActions.Redo)), "Redo")
