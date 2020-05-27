c4b2242d6a37dc85a0ef6e25b8fb32ea0c5792f8
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2020-05-27 15:25:02 +0200

    Cosmetics

--- src/main/scala/example/RecipesView.scala
+++ src/main/scala/example/RecipesView.scala
@@ -15,8 +15,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class RecipesView(recipes: ModelRO[Recipes], dispatch: Dispatcher) {
 
-  val r2 = Recipe("r2", "Couscous", Seq("ig12", "ig22"), Seq("is12", "is22"))
-  val r3 = Recipe("r3", "Creme Brulee", Seq("ig13", "ig23"), Seq("is13", "is23"))
+  val couscous = Recipe("couscous", "Couscous", Seq("ig12", "ig22"), Seq("is12", "is22"))
+  val cremeBrulee = Recipe("creme-brulee", "Creme Brulee", Seq("ig13", "ig23"), Seq("is13", "is23"))
 
   def fetchRecipes = Ajax.get("http://localhost:5000/recipes").onSuccess {
     case xhr =>
@@ -40,11 +40,15 @@ class RecipesView(recipes: ModelRO[Recipes], dispatch: Dispatcher) {
       div(
         cls := "btn-group",
         button(cls := "btn btn-default", onclick := (() => fetchRecipes), "Fetch"),
-        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(r2))), "Add Couscous"),
-        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(r3))), "Add Creme Brulee"),
-        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Reset)), "Reset"),
+        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(couscous))), "Add Couscous"),
+        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(cremeBrulee))), "Add Creme Brulee")
+      ),
+      p(),
+      div(
+        cls := "btn-group",
         button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Undo)), "Undo"),
-        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Redo)), "Redo")
+        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Redo)), "Redo"),
+        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Reset)), "Reset")
       )
     )
   }
