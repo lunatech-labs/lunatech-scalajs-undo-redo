40e53f08d0fdc02d69fc1c3340d3cbb0d880d357
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2020-05-27 15:18:58 +0200

    Add button to delete a recipe

--- src/main/scala/example/RecipesView.scala
+++ src/main/scala/example/RecipesView.scala
@@ -15,8 +15,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class RecipesView(recipes: ModelRO[Recipes], dispatch: Dispatcher) {
 
-  val r2 = Recipe("r2", "n2", Seq("ig12", "ig22"), Seq("is12", "is22"))
-  val r3 = Recipe("r3", "n3", Seq("ig13", "ig23"), Seq("is13", "is23"))
+  val r2 = Recipe("r2", "Couscous", Seq("ig12", "ig22"), Seq("is12", "is22"))
+  val r3 = Recipe("r3", "Creme Brulee", Seq("ig13", "ig23"), Seq("is13", "is23"))
 
   def fetchRecipes = Ajax.get("http://localhost:5000/recipes").onSuccess {
     case xhr =>
@@ -33,12 +33,15 @@ class RecipesView(recipes: ModelRO[Recipes], dispatch: Dispatcher) {
     div(
       h3("Cookbook"),
       p("Recipes: "),
-      ul(recipes().recipes.map { recipe => li(recipe.name)}),
+      ul(recipes().recipes.map { recipe => li(
+        span(recipe.name),
+        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Delete(recipe))), "Delete"),
+      )}),
       div(
         cls := "btn-group",
         button(cls := "btn btn-default", onclick := (() => fetchRecipes), "Fetch"),
-        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(r2))), "Add r2"),
-        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(r3))), "Add r3"),
+        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(r2))), "Add Couscous"),
+        button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Add(r3))), "Add Creme Brulee"),
         button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Reset)), "Reset"),
         button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Undo)), "Undo"),
         button(cls := "btn btn-default", onclick := (() => dispatch(RecipeActions.Redo)), "Redo")
