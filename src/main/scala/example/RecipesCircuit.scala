046df231de4d44307149b342a49fe04867edd4a0
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2020-05-27 16:05:43 +0200

    Added persistence logic in RecipeCircuit

--- src/main/scala/example/RecipesCircuit.scala
+++ src/main/scala/example/RecipesCircuit.scala
@@ -7,26 +7,49 @@ import io.circe.generic.semiauto._
 
 case class Recipe(id: String, name: String, ingredients: Seq[String], instructions: Seq[String])
 
-case class Recipes(recipes: Seq[Recipe], pastActions: List[RecipeAction], futureActions: List[RecipeAction])
+case class Recipes(
+  recipes: Seq[Recipe],
+  pastActions: List[ReversibleRecipeAction],
+  futureActions: List[ReversibleRecipeAction],
+  toAdd: List[Recipe],
+  toDelete: List[Recipe]
+)
 
 object Recipe {
   implicit val recipeDecoder: Decoder[Recipe] = deriveDecoder[Recipe]
   implicit val recipeEncoder: Encoder[Recipe] = deriveEncoder[Recipe]
 }
 
-trait RecipeAction extends Action {
+trait ReversibleRecipeAction extends Action {
   def update(model: Recipes): Recipes
   def undo(model: Recipes): Recipes
 }
 
 object RecipeActions {
 
-  case class Add(recipe: Recipe) extends RecipeAction {
-    def update(model: Recipes): Recipes = model.copy(recipes = recipe +: model.recipes)
-    def undo(model: Recipes): Recipes = model.copy(recipes = model.recipes.filterNot(_.id == recipe.id))
+  case class Add(recipe: Recipe) extends ReversibleRecipeAction {
+
+    def update(model: Recipes): Recipes = model.copy(
+      recipes = recipe +: model.recipes,
+      toAdd = recipe +: model.toAdd
+    )
+
+    def undo(model: Recipes): Recipes = {
+      if (model.toAdd.contains(recipe)) {
+        model.copy(
+          recipes = model.recipes.filterNot(_.id == recipe.id),
+          toAdd = model.toAdd.filterNot(_.id == recipe.id)
+        )
+      } else {
+        model.copy(
+          recipes = model.recipes.filterNot(_.id == recipe.id),
+          toDelete = recipe +: model.toDelete
+        )
+      }
+    }
   }
 
-  case class Delete(recipe: Recipe) extends RecipeAction {
+  case class Delete(recipe: Recipe) extends ReversibleRecipeAction {
     def update(model: Recipes): Recipes = model.copy(recipes = model.recipes.filterNot(_.id == recipe.id))
     def undo(model: Recipes): Recipes = model.copy(recipes = recipe +: model.recipes)
   }
@@ -39,7 +62,7 @@ object RecipeActions {
 
 object RecipesCircuit extends Circuit[Recipes] {
 
-  val initialModel = Recipes(Nil, Nil, Nil)
+  val initialModel = Recipes(Nil, Nil, Nil, Nil, Nil)
 
   override val actionHandler: HandlerFunction =
     (model, action) => action match {
@@ -64,7 +87,7 @@ object RecipesCircuit extends Circuit[Recipes] {
         Some(ModelUpdate(newModel))
       }
 
-      case action: RecipeAction => {
+      case action: ReversibleRecipeAction => {
         val newModel = action.update(model).copy(
           pastActions = action +: model.pastActions
         )
